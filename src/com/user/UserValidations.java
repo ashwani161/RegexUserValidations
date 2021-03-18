f0e472fc1b9a4b81ffd8da1ab68e8bff30bf7be9
ashwani161 <ashwanin65@gmail.com>
2021-03-18 17:16:56 +0530

    Added:User need to enter valid FirstName

--- src/com/user/UserValidations.java
+++ src/com/user/UserValidations.java
@@ -1,7 +1,24 @@
 package com.user;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserValidations {
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration system to ensure all Validations");
+	public boolean validateFirstName(String firstName) {
+		String regExp = "^[A-Z][a-zA-Z]{3,3}";
+		Pattern pattern = Pattern.compile(regExp);
+		Matcher matcher = pattern.matcher(firstName);
+		return matcher.matches();
 	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome to User Registration system to ensure all Validations");
+		System.out.println("Enter your first name");
+		String firstName = scanner.next();
+		boolean result = new UserValidations().validateFirstName(firstName);
+		System.out.println(result);
+	}
+
 }
