16556858168e2a2ac6d34d24d9d51b8ef10d328a
ashwani161 <ashwanin65@gmail.com>
2021-03-18 17:11:51 +0530

    Added:Welcome to User Validations

---
+++ src/com/user/UserValidations.java
@@ -0,0 +1,7 @@
+package com.user;
+
+public class UserValidations {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration system to ensure all Validations");
+	}
+}
