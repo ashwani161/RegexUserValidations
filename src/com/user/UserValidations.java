3b0d252463166b78d495adeccb4c254fb3bf636c
ashwani161 <ashwanin65@gmail.com>
2021-03-18 17:32:55 +0530

    Added:User need to enter valid LastName

--- src/com/user/UserValidations.java
+++ src/com/user/UserValidations.java
@@ -11,14 +11,24 @@ public class UserValidations {
 		Matcher matcher = pattern.matcher(firstName);
 		return matcher.matches();
 	}
+	public boolean validateLastName(String lastName) {
+		String regExp = "^[A-Z][a-zA-Z]{3,3}";
+		Pattern pattern = Pattern.compile(regExp);
+		Matcher matcher = pattern.matcher(lastName);
+		return matcher.matches();
+	}
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to User Registration system to ensure all Validations");
 		System.out.println("Enter your first name");
+		System.out.println("Enter your last name");
 		String firstName = scanner.next();
+		String lastName = scanner.next();
 		boolean result = new UserValidations().validateFirstName(firstName);
-		System.out.println(result);
+		boolean result1 = new UserValidations().validateLastName(lastName);
+        System.out.println(result);
+        System.out.println(result1);
 	}
 
 }
