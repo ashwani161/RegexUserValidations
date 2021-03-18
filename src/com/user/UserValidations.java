578afcdc9b484b5d6627c955f668972ab955ac63
ashwani161 <ashwanin65@gmail.com>
2021-03-18 20:43:40 +0530

    Added:User need to follow mobile format with country code and space 10 digits

--- src/com/user/UserValidations.java
+++ src/com/user/UserValidations.java
@@ -5,6 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidations {
+
 	public boolean validateFirstName(String firstName) {
 		String regExp = "^[A-Z][a-zA-Z]{3,3}";
 		Pattern pattern = Pattern.compile(regExp);
@@ -13,16 +14,19 @@ public class UserValidations {
 	}
 
 	public boolean validateLastName(String lastName) {
-		String regExp = "^[A-Z][a-zA-Z]{3,3}";
-		Pattern pattern = Pattern.compile(regExp);
+		Pattern pattern = Pattern.compile("^[A-Z][a-zA-Z]{3,3}");
 		Matcher matcher = pattern.matcher(lastName);
 		return matcher.matches();
 	}
 
 	public boolean validateEmail(String email) {
-		String regExp = "^[A-Za-z0-9+_.-]+@(.+)$";
-		Pattern pattern = Pattern.compile(regExp);
+		Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
 		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	public boolean validateMobileFormat(String mobileFormat) {
+		Pattern pattern = Pattern.compile("^(?:[0-9] ?{6,14}[0-9]$)");
+		Matcher matcher = pattern.matcher(mobileFormat);
 		return matcher.matches();
 	}
 
@@ -32,15 +36,19 @@ public class UserValidations {
 		System.out.println("Enter your first name");
 		System.out.println("Enter your last name");
 		System.out.println("Enter your email");
+		System.out.println("Enter your mobile format");
 		String firstName = scanner.next();
 		String lastName = scanner.next();
 		String email = scanner.next();
+		String mobileFormat = scanner.next();
 		boolean result = new UserValidations().validateFirstName(firstName);
 		boolean result1 = new UserValidations().validateLastName(lastName);
 		boolean result2 = new UserValidations().validateEmail(email);
+		boolean result3 = new UserValidations().validateMobileFormat(mobileFormat);
 		System.out.println(result);
 		System.out.println(result1);
 		System.out.println(result2);
+		System.out.println(result3);
 	}
 
 }
